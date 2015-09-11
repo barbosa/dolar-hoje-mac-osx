b40a47b1e767c6d495e5750cfef2a12d31068de9
Gustavo Barbosa <gustavocsb@gmail.com>
2015-09-10 21:26:24 -0400

    Initial Commit

---
+++ DolarHoje/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DolarHoje
+//
+//  Created by Gustavo Barbosa on 9/10/15.
+//  Copyright © 2015 Gustavo Barbosa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
