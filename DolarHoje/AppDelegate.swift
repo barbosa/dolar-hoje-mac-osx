c63decf08e2368bda7d3d8e0b438148b0df1b75f
Gustavo Barbosa <gustavocsb@gmail.com>
2015-09-10 23:32:59 -0400

    initial commit

--- DolarHoje/AppDelegate.swift
+++ DolarHoje/AppDelegate.swift
@@ -7,21 +7,36 @@
 //
 
 import Cocoa
+import Alamofire
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    var statusItem: NSStatusItem?
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+        statusItem?.menu = statusMenu
+        statusItem?.highlightMode = true
+        
+        fetch()
+        NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: "fetch", userInfo: nil, repeats: true)
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    func fetch() {
+        Alamofire.request(.GET, "http://api.dolarhoje.com")
+            .responseString { _, _, result in
+                if let value = result.value {
+                    print("\(value)")
+                    self.statusItem?.title = "R$ \(value)"
+                }
+        }
     }
-
-
+    
+    @IBAction func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
 }
 
